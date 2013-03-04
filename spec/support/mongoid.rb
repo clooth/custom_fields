b874e051dbb43ad0c1749414b9abf4c5f2c59b27
Duilio Ruggiero <duilio.ruggiero@gmail.com>
2013-03-05 00:00:16 +0100

    use connect_to to connect to mongodb in test

--- spec/support/mongoid.rb
+++ spec/support/mongoid.rb
@@ -2,9 +2,7 @@ require 'mongoid'
 
 Mongoid.configure do |config|
   name = 'custom_fields_test'
-  host = 'localhost'
-  config.master = Mongo::Connection.new.db(name)
-  # config.master = Mongo::Connection.new('localhost', '27017', :logger => Logger.new($stdout)).db(name)
+  config.connect_to(name)
 end
 
 module Mongoid
